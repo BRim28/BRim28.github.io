140f977fd55b94291d4cfa0a2f8f74313c7e221b
BRim28 <39436874+BRim28@users.noreply.github.com>
2026-01-19 20:25:07 +0800

    Updated vite_config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/BRim28.github.io/',
   plugins: [react()],
-})
+  base: '/BRim28.github.io/',
+});
